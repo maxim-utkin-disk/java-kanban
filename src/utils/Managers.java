4e7b2994bf4d39280c9c7c4345acbf3a3e80831a
Maxim Utkin <maxim-utkin-disk@yandex.ru>
2025-02-02 20:35:49 +0300

    ТЗ-6: устраняем замечания ГитХаба #3

--- src/utils/Managers.java
+++ src/utils/Managers.java
@@ -7,7 +7,8 @@ import controllers.TaskManager;
 
 public class Managers {
 
-    private Managers() {}; //чтобы не создавали экземпляр этого класса, использовать только статич.методы
+    private Managers() {
+    }; //чтобы не создавали экземпляр этого класса, использовать только статич.методы
 
     public static TaskManager getDefaultManager(){
         return new InMemoryTaskManager();
